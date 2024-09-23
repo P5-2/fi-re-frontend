88067b1cc8197d26b5bbfaee580fb67a13365c77
platypus3036 <akakehcn@gmail.com>
2024-09-23 15:22:35 +0900

    feat:front merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,13 @@
-import { createWebHistory, createRouter } from "vue-router"; // npm i vue-router@next
+import { createWebHistory, createRouter } from 'vue-router'; // npm i vue-router@next
 
-import login from "@/router/login/login.js"
-import HotItemList from "@/router/hotItem/hotItemList.js"
-import itemDetail from './itemDetail/itemDetail.js'
+import login from '@/router/login/login.js';
+import HotItemList from '@/router/hotItem/hotItemList.js';
+import itemDetail from './itemDetail/itemDetail.js';
+import GoldPrice from '@/router/gold/gold.js';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        ...login,
-        ...HotItemList,
-        ...itemDetail
-    ]
-})
+  history: createWebHistory(),
+  routes: [...login, ...HotItemList, ...itemDetail, ...GoldPrice],
+});
 
 export default router;
