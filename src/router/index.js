52070d073a127cfffc4271a350fa5bf251d09a54
oneul0 <gihoon1011@gmail.com>
2024-09-26 11:30:52 +0900

    Feat: 맞춤 상품 페이지 구현
    - 사용자 인증을 통한 데이터 송수신 기능
    - 받아온 데이터가 없을때 설문조사 진행 유도 버튼 추가

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,9 @@ import GoldPrice from "@/router/gold/gold.js";
 import Forex from "@/router/forex/forex.js";
 import SavingsList from "@/router/list/savingsList.js";
 import fund from "@/router/list/fund.js";
-
-<<<<<<< HEAD
-import login from '@/router/login/login.js';
-import HotItemList from '@/router/hotItem/hotItemList.js';
-import itemDetail from './itemDetail/itemDetail.js';
-import GoldPrice from '@/router/gold/gold.js';
-import fund from '@/router/list/fund.js';
 import Survey from './survey/survey.js';
-import Forex from '@/router/forex/forex.js';
-=======
->>>>>>> 033a2666797bf1763e4372404b2e14013ab95a83
+import RecommendationPage from "@/router/recommendation/recommendation.js";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -28,11 +20,9 @@ const router = createRouter({
     ...Forex,
     ...SavingsList,
     ...fund,
-<<<<<<< HEAD
     ...Survey,
     ...Forex,
-=======
->>>>>>> 033a2666797bf1763e4372404b2e14013ab95a83
+    ...RecommendationPage,
   ],
 });
 
