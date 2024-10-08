54f5e2325c4ff37adfe92c5930a137046752f044
kUZEEwon <pink7519@pusan.ac.kr>
2024-10-08 17:12:43 +0900

    Feat: 예적금 리스트 즐겨찾기 추가 기능

--- src/pages/list/savingsDeposit/SavingsDepositList.vue
+++ src/pages/list/savingsDeposit/SavingsDepositList.vue
@@ -31,13 +31,6 @@
         다음
       </button>
     </div>
-
-    <!-- 비교 모달 컴포넌트 -->
-    <ComparisonModal
-      :selectedSavingsDeposit="selectedSavingsDeposit"
-      :isComparisonModalVisible="isComparisonModalVisible"
-      @close="closeModalAndResetSavingsDeposit"
-    />
   </div>
 </template>
 
@@ -45,13 +38,11 @@
 import { ref, onMounted } from "vue";
 import axios from "axios";
 import SavingsDeposit from "@/components/list/savingsDeposit/SavingsDeposit.vue";
-import ComparisonModal from "@/components/comparison/ComparisonModal.vue";
 import FilterButtons from "@/components/list/savingsDeposit/FilterButtons.vue";
 
 export default {
   components: {
     SavingsDeposit,
-    ComparisonModal,
     FilterButtons,
   },
   setup() {
